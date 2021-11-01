62428085e3db63d0e97dbb46b2201ba84b38fc07
Victor Hoffmann <viictorhoffmann@gmail.com>
2021-11-01 15:23:24 -0300

    Endpoint para retornar um post por id

---
+++ src/main/java/com/victorhoffmann/mongospring/services/PostService.java
@@ -0,0 +1,22 @@
+package com.victorhoffmann.mongospring.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.victorhoffmann.mongospring.domain.Post;
+import com.victorhoffmann.mongospring.repository.PostRepository;
+import com.victorhoffmann.mongospring.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
