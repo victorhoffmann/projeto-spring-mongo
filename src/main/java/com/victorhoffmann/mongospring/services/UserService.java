2757ed27b02d40731884799641f44b62bbc02043
Victor Hoffmann <viictorhoffmann@gmail.com>
2021-11-01 14:04:14 -0300

    Obtendo User pelo ID

--- src/main/java/com/victorhoffmann/mongospring/services/UserService.java
+++ src/main/java/com/victorhoffmann/mongospring/services/UserService.java
@@ -1,20 +1,27 @@
 package com.victorhoffmann.mongospring.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.victorhoffmann.mongospring.domain.User;
 import com.victorhoffmann.mongospring.repository.UserRepository;
+import com.victorhoffmann.mongospring.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
---
+++ src/main/java/com/victorhoffmann/mongospring/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.victorhoffmann.mongospring.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
