897d338b46a093391218e6487c2f5ff36bcceec7
Victor Hoffmann <viictorhoffmann@gmail.com>
2021-11-01 12:12:42 -0300

    Conectando com MongoDB

--- src/main/java/com/victorhoffmann/mongospring/resources/UserResource.java
+++ src/main/java/com/victorhoffmann/mongospring/resources/UserResource.java
@@ -1,26 +1,26 @@
 package com.victorhoffmann.mongospring.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.victorhoffmann.mongospring.domain.User;
+import com.victorhoffmann.mongospring.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1001", "Maria Brown", "maria@gmail.com");
-		User alex = new User("1002", "Alex Green", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria,alex));
+		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 }
