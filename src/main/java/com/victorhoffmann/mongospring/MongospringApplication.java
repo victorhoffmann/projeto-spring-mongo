1efbd557948bade0654be3c3941dcee97d918a81
Victor Hoffmann <viictorhoffmann@gmail.com>
2021-11-01 11:40:18 -0300

    Inicializando projeto Spring Boot com MongoDB

---
+++ src/main/java/com/victorhoffmann/mongospring/MongospringApplication.java
@@ -0,0 +1,13 @@
+package com.victorhoffmann.mongospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorhoffmann/mongospring/MongospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorhoffmann.mongospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
